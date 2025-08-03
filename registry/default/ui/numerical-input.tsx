420959d027f15dc232d762e5a9af5dd0f30bf109
cipher0 <cipher0.dev@outlook.com>
2025-08-03 10:27:10 +0800

    feat: prettier

--- registry/default/ui/numerical-input.tsx
+++ registry/default/ui/numerical-input.tsx
@@ -1,46 +1,51 @@
-import * as React from 'react'
-import { Input } from '@/components/ui/input'
+import * as React from "react";
+import { Input } from "@/components/ui/input";
 
-const inputRegex = RegExp(`^\\d*(?:\\\\[.])?\\d*$`)
+const inputRegex = RegExp(`^\\d*(?:\\\\[.])?\\d*$`);
 
 const escapeRegExp = (string: string): string => {
-    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+};
+
+export interface NumericalInputProps
+  extends Omit<
+    React.ComponentProps<typeof Input>,
+    "onChange" | "value" | "type"
+  > {
+  value: string | number | undefined;
+  onChange: (value: string) => void;
 }
 
-export interface NumericalInputProps extends Omit<React.ComponentProps<typeof Input>, 'onChange' | 'value' | 'type'> {
-    value: string | number | undefined
-    onChange: (value: string) => void
-}
+const NumericalInput = React.forwardRef<
+  React.ElementRef<typeof Input>,
+  NumericalInputProps
+>(({ className, value, onChange, placeholder = "0.0", ...props }, ref) => {
+  const enforcer = (nextUserInput: string) => {
+    if (nextUserInput === "" || inputRegex.test(escapeRegExp(nextUserInput))) {
+      onChange(nextUserInput);
+    }
+  };
 
-const NumericalInput = React.forwardRef<React.ElementRef<typeof Input>, NumericalInputProps>(
-    ({ className, value, onChange, placeholder = '0.0', ...props }, ref) => {
-        const enforcer = (nextUserInput: string) => {
-            if (nextUserInput === '' || inputRegex.test(escapeRegExp(nextUserInput))) {
-                onChange(nextUserInput)
-            }
-        }
+  return (
+    <Input
+      ref={ref}
+      className={className}
+      value={value}
+      onChange={(event) => {
+        enforcer(event.target.value.replace(/,/g, "."));
+      }}
+      inputMode="decimal"
+      autoComplete="off"
+      autoCorrect="off"
+      type="text"
+      pattern="^[0-9]*[.,]?[0-9]*$"
+      placeholder={placeholder}
+      spellCheck="false"
+      {...props}
+    />
+  );
+});
 
-        return (
-            <Input
-                ref={ref}
-                className={className}
-                value={value}
-                onChange={(event) => {
-                    enforcer(event.target.value.replace(/,/g, '.'))
-                }}
-                inputMode="decimal"
-                autoComplete="off"
-                autoCorrect="off"
-                type="text"
-                pattern="^[0-9]*[.,]?[0-9]*$"
-                placeholder={placeholder}
-                spellCheck="false"
-                {...props}
-            />
-        )
-    },
-)
+NumericalInput.displayName = "NumericalInput";
 
-NumericalInput.displayName = 'NumericalInput'
-
-export { NumericalInput }
+export { NumericalInput };
